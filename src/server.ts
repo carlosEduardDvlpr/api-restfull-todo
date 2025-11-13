4e3a1266ec8d20f20fb68862d51fee422db01e18
TechnoGET <atendimento.technoget@gmail.com>
2025-11-12 23:10:12 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log('Server is running on port 3333');
+});
