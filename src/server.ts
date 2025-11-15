de01bc674a498b7b621f8c8ba641c92ef07bc9dc
TechnoGET <atendimento.technoget@gmail.com>
2025-11-14 21:42:27 -0300

    initial server.ts archive

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
 import express from 'express';
 
+const PORT = process.env.PORT || 3333;
+
 const app = express();
 app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-  console.log('Server is running on port 3333');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
