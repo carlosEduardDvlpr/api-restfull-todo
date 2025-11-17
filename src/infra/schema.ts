e22f61fd7cb16c3335a38c8a4aba5d0a68aba3b0
TechnoGET <atendimento.technoget@gmail.com>
2025-11-17 00:31:31 -0300

    alterado nome da tabela do banco de todo para tasks

--- src/infra/schema.ts
+++ src/infra/schema.ts
@@ -6,20 +6,25 @@ export const usersSchema = sqliteTable('users', {
   password: text('password', { length: 255 }).notNull(),
 });
 
-export const todoListSchema = sqliteTable('todo_list', {
+export const tasksSchema = sqliteTable('tasks', {
   id: int('id').primaryKey({ autoIncrement: true }).notNull(),
   user_id: int('user_id')
     .references(() => usersSchema.id)
     .notNull(),
   title: text('title', { length: 60 }).notNull(),
-  description: text('title', { length: 255 }).notNull(),
-  status: text({ enum: ['TODO', 'DOING', 'DONE'] })
+  description: text('description', { length: 255 }).notNull(),
+  status: text({ enum: ['TODO', 'DONE'] })
     .default('TODO')
     .notNull(),
   created_at: text('created_at').default(
-    new Date().toLocaleDateString('pt-br'),
+    new Date().toLocaleString('pt-br', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }),
   ),
-  updated_at: text('updated_at').default(
-    new Date().toLocaleDateString('pt-br'),
-  ),
+  updated_at: text('updated_at'),
 });
