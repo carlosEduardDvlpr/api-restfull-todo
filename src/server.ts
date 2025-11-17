87327a2c55b287eeefab4a781c6cb8a223b80b64
TechnoGET <atendimento.technoget@gmail.com>
2025-11-17 00:31:55 -0300

    arquivo server inicial, ocultado valor da porta para variavel de ambiente

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import 'dotenv/config';
 import express from 'express';
+import { routes } from './routes';
+import { errorHandling } from './middleware/error-handling';
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT;
 
 const app = express();
 app.use(express.json());
+
+app.use(routes);
+app.use(errorHandling);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
